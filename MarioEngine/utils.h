7062045629242f0b353d54ef271eb9bcdc577628
Tex <atexier.at@gmail.com>
2021-02-13 14:46:43 +0100

    Initial SDL project
    First commit of the base of the project.
    It's a Visual Studio project with SDL2

---
+++ MarioEngine/utils.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+#define max(x,y)	((x)>(y)?(x):(y))
+#define min(x,y)	((x)<(y)?(x):(y))
